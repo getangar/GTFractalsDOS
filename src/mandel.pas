d81b4cc15e854c28dc4ede91b9d72c4f3e230442
Gennaro Eduardo Tangari <gennaro.tangari@outlook.com>
2025-05-19 10:53:45 +0200

    Uploading Turbo Pascal 6.0 code and some screenshots

---
+++ src/mandel.pas
@@ -0,0 +1,154 @@
+{$N+}
+PROGRAM MandelbrotSet;
+
+USES
+  Crt, Graph;
+
+CONST
+  ESCAPE_RADIUS = 2.0;
+
+VAR
+  gDriver, gMode: INTEGER;
+  x, y: INTEGER;
+  realPart, imagPart: REAL;
+  realMin, realMax, imagMin, imagMax: REAL;
+  iter: INTEGER;
+  zReal, zImag, tempReal: REAL;
+  screenWidth, screenHeight: INTEGER;
+  realScale, imagScale: REAL;
+  paramString: STRING;
+  paramValue: REAL;
+  paramCode: INTEGER;
+  maxIterations: INTEGER;
+  iterations: INTEGER;
+
+PROCEDURE Beep;
+BEGIN
+  Sound(1000);
+  Delay(500);
+  NoSound;
+END;
+
+PROCEDURE Header;
+BEGIN
+  WriteLn('GTFractals for MS-DOS');
+  Writeln('(c)Copyright 1992-2025 by Gennaro Eduardo Tangari.');
+  Writeln;
+END;
+
+PROCEDURE ShowParameters;
+BEGIN
+  Writeln; Writeln('Here the provided parameters:');
+  Writeln('X-Min: ', realMin);
+  Writeln('X-Max: ', realMax);
+  Writeln('Y-Min: ', imagMin);
+  Writeln('Y-Max: ', imagMax);
+  Writeln('Iteractions: ', maxIterations);
+  Writeln;
+END;
+
+BEGIN
+  ClrScr; Beep; Header;
+
+  Write('X-Min (default = -2.25): ');
+  Readln(paramString);
+
+  IF paramString = '' THEN
+    realMin := -2.25
+  ELSE
+  BEGIN
+    Val(paramString, paramValue, paramCode);
+    realMin := paramValue;
+  END;
+
+  Write('X-Max (default = 0.75): ');
+  Readln(paramString);
+
+  IF paramString = '' THEN
+    realMax := 0.75
+  ELSE
+  BEGIN
+    Val(paramString, paramValue, paramCode);
+    realMax := paramValue;
+  END;
+
+  Write('Y-Min (default = -1.5): ');
+  Readln(paramString);
+
+  IF paramString = '' THEN
+    imagMin := -1.5
+  ELSE
+  BEGIN
+    Val(paramString, paramValue, paramCode);
+    imagMin := paramValue;
+  END;
+
+  Write('Y-Max (default = 1.5): ');
+  Readln(paramString);
+
+  IF paramString = '' THEN
+    imagMax := 1.5
+  ELSE
+  BEGIN
+    Val(paramString, paramValue, paramCode);
+    imagMax := paramValue;
+  END;
+
+  Write('Iterations (default = 150): ');
+  Readln(paramString);
+
+  IF paramString = '' THEN
+    maxIterations := 150
+  ELSE
+  BEGIN
+    Val(paramString, iterations, paramCode);
+    maxIterations := iterations;
+  END;
+
+  ShowParameters;
+
+  WriteLn('Press <ENTER> to start');
+  ReadLn;
+  gDriver := Detect;
+  gMode := VGAMed;
+  InitGraph(gDriver, gMode, 'C:\SOFTWARE\TP\BGI');
+
+
+  screenWidth := GetMaxX;
+  screenHeight := GetMaxY;
+
+  realScale := (realMax - realMin) / screenWidth;
+  imagScale := (imagMax - imagMin) / screenHeight;
+
+  FOR y := 0 TO screenHeight DO
+  BEGIN
+    imagPart := imagMin + y * imagScale;
+
+    FOR x := 0 TO screenWidth DO
+    BEGIN
+      realPart := realMin + x * realScale;
+
+      zReal := 0.0;
+      zImag := 0.0;
+      iter := 0;
+
+      WHILE (Sqr(zReal) + Sqr(zImag) <= Sqr(ESCAPE_RADIUS)) AND (iter < maxIterations) DO
+      BEGIN
+        tempReal := Sqr(zReal) - Sqr(zImag) + realPart;
+        zImag := 2.0 * zReal * zImag + imagPart;
+        zReal := tempReal;
+        Inc(iter);
+      END;
+
+      IF iter = maxIterations THEN
+        PutPixel(x, y, 0)
+      ELSE
+        PutPixel(x, y, (iter MOD 15) + 1);
+    END;
+  END;
+
+  Beep; ReadLn;
+  CloseGraph;
+  ClrScr;
+  WriteLn('Thanks for using GTFractals!');
+END.
